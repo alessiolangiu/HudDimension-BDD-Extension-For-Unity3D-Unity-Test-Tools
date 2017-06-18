9c2eddfb63961118f00a412bea0953e1751746c6
alessiolangiu <alessio.langiu@gmail.com>
2017-06-18 14:34:49 +0100

    Some refactoring, align the file names with class names, cleaning the file header documentation

--- Assets/UnityTestToolsBDDExtension/Assertions/Editor/ExtensionRunnerEditorBusinessLogic/RunnerEditorBusinessLogicStaticRows.cs
+++ Assets/UnityTestToolsBDDExtension/Assertions/Editor/ExtensionRunnerEditorBusinessLogic/RunnerEditorBusinessLogicStaticRows.cs
@@ -1,18 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="RunnerEditorBusinessLogicStaticRows.cs" company="Hud Dimesion">
-//     Copyright (c) Hud Dimension. All rights reserved.
-// </copyright>
-//
-// <disclaimer>
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-// </disclaimer>
-//
-// <author>Alessio Langiu</author>
-// <email>alessio.langiu@huddimension.co.uk</email>
-//-----------------------------------------------------------------------
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
